2b13599ce40849389947b976781980e067875098
Olivier Huin <olih@users.noreply.github.com>
2023-06-08 09:22:52 +0100

    tweaks model

--- src/prism-beam-model.ts
+++ src/prism-beam-model.ts
@@ -7,31 +7,42 @@ const urlTemplate = stringFields.string1To140;
 const roles = z.array(stringFields.string1To80).min(1).optional();
 
 const getHttpAction = z.object({
-  kind: z.literal('get'),
+  kind: z.literal('http/get'),
   contentType: z.enum(contentTypes),
   schemaName: stringFields.string1To50,
   urlTemplate,
-  tags,
-  roles,
 });
 
 const postHttpAction = z.object({
-  kind: z.literal('post'),
+  kind: z.literal('http/post'),
   encodingType: z.enum(contentTypes),
   schemaName: stringFields.string1To50,
   urlTemplate,
-  tags,
-  roles,
 });
 
-const target = z.discriminatedUnion('kind', [getHttpAction, postHttpAction]);
+const putHttpAction = z.object({
+  kind: z.literal('http/put'),
+  encodingType: z.enum(contentTypes),
+  schemaName: stringFields.string1To50,
+  urlTemplate,
+});
+
+const target = z.discriminatedUnion('kind', [
+  getHttpAction,
+  postHttpAction,
+  putHttpAction,
+]);
 
 const action = z.object({
   name: stringFields.string1To50,
   target,
+  tags,
+  roles,
 });
 
 export const entity = z.object({
   id: stringFields.string1To140,
   _actions: z.array(action).min(1).optional(),
 });
+
+export type PrismBeamEntity = z.infer<typeof entity>;
