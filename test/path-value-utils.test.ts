22e364b88b08cfd887529c7f4b0d762c7c7f6efc
Olivier Huin <olih@users.noreply.github.com>
2023-06-10 14:37:31 +0100

    Adds array, boolean and number if entity

--- test/path-value-utils.test.ts
+++ test/path-value-utils.test.ts
@@ -11,6 +11,9 @@ test('entityToPathValueList and pathValueListToEntity should get back the initia
     id: 'id123',
     name: 'name123',
     address: {street: '123 street'},
+    active: true,
+    age: 21,
+    phones: ['phone1', 'phone2', 'phone3'],
   };
   const paths = getEntityPaths(entity);
   const pvl = entityToPathValueList(entity, paths);
