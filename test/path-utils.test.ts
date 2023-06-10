2cf8a9d52a1bf08831db06a9a42bde5193c37a4a
Olivier Huin <olih@users.noreply.github.com>
2023-06-10 10:46:44 +0100

    Ensure dot prop

--- test/path-utils.test.ts
+++ test/path-utils.test.ts
@@ -2,13 +2,13 @@ import {test} from 'node:test';
 import assert from 'node:assert/strict';
 import {arePathsInAllowList} from '../src/path-utils.js';
 
-const checkAbc = arePathsInAllowList(new Set(['a/b/c', 'f/g/h', 'f/g/*/1']));
+const checkAbc = arePathsInAllowList(new Set(['a.b.c', 'f.g.h', 'f.i.*.a.b']));
 
 test('arePathsInAllowList should accept expected path', () => {
-  assert.ok(checkAbc(['f/g/h']), 'fgh');
-  assert.ok(checkAbc(['f/g/h', 'a/b/c']), 'fgh and abc');
+  assert.ok(checkAbc(['f.g.h']), 'fgh');
+  assert.ok(checkAbc(['f.g.h', 'a.b.c']), 'fgh and abc');
 });
 
 test('arePathsInAllowList should not accept bad paths', () => {
-  assert.ok(checkAbc(['wrong/path']), 'wrong path');
+  assert.equal(checkAbc(['wrong.path']), false, 'wrong path');
 });
