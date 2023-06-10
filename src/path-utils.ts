c8ccd85d09059896d314d0d9a44b5b357e182276
Olivier Huin <olih@users.noreply.github.com>
2023-06-10 11:11:33 +0100

    Adds isSafePath

--- src/path-utils.ts
+++ src/path-utils.ts
@@ -3,7 +3,10 @@ import {type PrismBeamBaseEntity} from './prism-beam-model.js';
 
 export const getEntityPaths = (entity: PrismBeamBaseEntity) => deepKeys(entity);
 
+const isSafePath = (path: string): boolean =>
+  /^\w+(\[\d+])?(\.\w+(\[\d+])?)*$/.test(path);
+
 export const arePathsInAllowList =
   (allowList: Set<string>) =>
   (actual: string[]): boolean =>
-    !actual.some((value) => !allowList.has(value));
+    !actual.some((path) => isSafePath(path) && !allowList.has(path));
