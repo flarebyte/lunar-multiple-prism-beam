aca6d624f8b52e3b3ae0053df04cf45bbc67c45d
Olivier Huin <olih@users.noreply.github.com>
2023-06-12 08:28:12 +0100

    Adds payload refs

--- src/prism-beam-model.ts
+++ src/prism-beam-model.ts
@@ -5,14 +5,6 @@ const tags = z.array(stringFields.string1To80).nonempty().max(30).optional();
 const role = stringFields.string1To80;
 const userId = stringFields.string1To80;
 const dateTime = z.date();
-const operation = z.enum([
-  'read',
-  'create',
-  'update',
-  'upsert',
-  'delete',
-  'search',
-]);
 
 export const entity = z.object({
   id: stringFields.string1To140,
@@ -26,19 +18,30 @@ const pathValue = z.object({
 const queryContext = z.object({
   userId,
   created: dateTime,
+  tags,
+});
+
+const ref = z.object({
+  name: stringFields.string1To80,
+  ref: stringFields.string1To80,
+});
+
+const payloadRef = z.object({
+  a: z.literal('entity'),
+  name: stringFields.string1To80,
+  entity,
 });
 
 const query = z.object({
   role,
-  operation,
   tags,
-  ids: z.array(z.number()).nonempty().max(30),
+  refs: z.array(ref).nonempty().max(30),
 });
 
 const task = z.object({
   context: queryContext,
   queries: z.array(query).nonempty(),
-  entities: z.array(entity).nonempty(),
+  payloads: z.array(payloadRef).nonempty(),
 });
 
 export type PrismBeamBaseEntity = z.infer<typeof entity>;
